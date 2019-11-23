a735aba54cdd2c9ff71f1ead92055850273d998b
jairoMolina9 <molinaandres9991@gmail.com>
2019-11-22 19:47:34 -0500

    prob 4

--- midterm2/prob4.java
+++ midterm2/prob4.java
@@ -2,6 +2,10 @@ import java.util.ArrayList;
 
 public class prob4{
   public static void main (String [] args) {
+    partC();
+  }
+
+  public static void partB() {
     ArrayList<String> carModel = new ArrayList<String>();
     carModel.add("BMW");
     carModel.add("Nissan");
@@ -10,6 +14,17 @@ public class prob4{
     carModel.remove(1);
     carModel.add(0,"Prius");
     carModel.set(2,"Ferrari");
-
   }
+
+  public static void partC() {
+    try{
+      String list [] = new String[10];
+      System.out.println(list[10]);
+    } catch (ArithmeticException e) {
+      System.out.println("A.E error");
+    } catch (RuntimeException e) {
+      System.out.println("R.E error");
+    }
+  }
+
 }
